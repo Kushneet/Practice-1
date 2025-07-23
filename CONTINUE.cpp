316001a45d7d340fd71c1cdfd7ce54928efb633f
Kushneet <kushneet1001@gmail.com>
2025-07-23 16:50:55 +0530

    Add files via upload

---
+++ CONTINUE.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+  for(int i=1;i<3;i++){
+    cout<<"Hello World\n";
+    cout<<"Hello Worlds\n";
+    continue;
+      cout<<"Hello Worlsds\n";
+      cout<<"Hello Wosssrlds\n";
+  }
+    return 0;
+}
